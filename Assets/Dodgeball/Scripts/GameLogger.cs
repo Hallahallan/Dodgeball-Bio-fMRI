97288d6c99ed66328a22778e128b12d2eadbbdcd
Kamomilla <kmgodlun@stud.ntnu.no>
2023-11-06 17:18:03 +0100

    Add logs for position and rotation to agents

--- Assets/Dodgeball/Scripts/GameLogger.cs
+++ Assets/Dodgeball/Scripts/GameLogger.cs
@@ -4,9 +4,9 @@ using System.IO;
 
 public class GameLogger : MonoBehaviour
 {
-    
+
     public DodgeBallGameController gameController;
-    
+
     public int winner;
     public int blueLives;
     public int purpleLives;
@@ -18,22 +18,26 @@ public class GameLogger : MonoBehaviour
     public int thrower;
     public string fileNameResults;
     public string fileNamePlayerData;
+    public string fileNamePosition;
     public string winstr;
 
     public void Start()
     {
         fileNameResults = "GameLog_Results_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".txt";
         fileNamePlayerData = "GameLog_Player_Data_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".txt";
+        fileNamePosition = "GameLog_Position_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".txt";
+
+        InvokeRepeating("LogPosition", 2.0f, 1.0f);
     }
     public void LogGameInfo()
     {
         string timestamp = System.DateTime.Now.ToString("HH:mm:ss.fff");
-        
+
         // Change winner to blue and purple instead of 0 and 1
         if (winner == 0) { winstr = "Blue"; }
-        else if (winner == 1) { winstr = "Purple"; }  
+        else if (winner == 1) { winstr = "Purple"; }
         else { winstr = "Draw"; }
-        
+
         string logMessage = "Time: " + timestamp + ", Winner: " + winstr + ", Blue lives left: " + blueLives + ", Purple lives left: " + purpleLives;
         // Create the "Logs" folder if it doesn't already exist
         string folderPath = Path.Combine(Application.dataPath, "Dodgeball/Logs/Results");
@@ -59,7 +63,7 @@ public class GameLogger : MonoBehaviour
             Directory.CreateDirectory(folderPath);
         }
         string path = Path.Combine(folderPath, fileNamePlayerData);
-        
+
         // Get the purple agent transform using GameController
         Transform purpleAgentTransform = gameController.Team1Players[0].Agent.transform;
 
@@ -74,7 +78,7 @@ public class GameLogger : MonoBehaviour
                 writer.WriteLine("Timestamp,EventType,BallsLeft,PlayerLives,EnemyLives,Corner");
                 writer.WriteLine($"{timestamp},{"Init"},{0},{3},{3},{cornerIndex}");
             }
-            
+
             string eventType = "";
             switch (n)
             {
@@ -110,6 +114,33 @@ public class GameLogger : MonoBehaviour
             _latestPurpleLives = gameController.Team1Players[0].Agent.HitPointsRemaining;
 
             writer.WriteLine($"{timestamp},{eventType},{_latestBlueBalls},{_latestBlueLives},{_latestPurpleLives},{cornerIndex}");
+        }
+    }
+
+    public void LogPosition()
+    {
+
+        // Create the "Logs" folder if it doesn't already exist
+        string folderPath = Path.Combine(Application.dataPath, "Dodgeball/Logs/Position");
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+        string path = Path.Combine(folderPath, fileNamePosition);
+
+        using (StreamWriter writer = File.AppendText(path))
+        {
+            // Write column names if the file is empty
+            if (new FileInfo(path).Length == 0)
+            {
+                writer.WriteLine("Timestamp,(Position_Blue_X,Position_Blue_Y),Rotation_Blue,(Position_Purple_X,Position_Purple_Y),Rotation_Purple");
+            }
+            string timestamp = System.DateTime.Now.ToString("HH:mm:ss.fff");
+            string agentBluePosition = $"({gameController.Team0Players[0].Agent.transform.position.x.ToString()},{gameController.Team0Players[0].Agent.transform.position.z.ToString()}),{gameController.Team0Players[0].Agent.transform.rotation.eulerAngles.y}";
+            string agentPurplePosition = $"({gameController.Team1Players[0].Agent.transform.position.x.ToString()},{gameController.Team1Players[0].Agent.transform.position.z.ToString()}),{gameController.Team1Players[0].Agent.transform.rotation.eulerAngles.y}";
+            //string testVar = gameController.Team1Players[0].Agent.transform.rotation;
+
+            writer.WriteLine($"{timestamp},{agentBluePosition},{agentPurplePosition}");
         }
     }
 }
